ecf0ec35748d611176ae419b743bcd6ab3d65dc3
Matokosp <jmsalinas@uc.cl>
2019-06-17 23:28:43 -0400

    proyectos update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,6 @@
+const sm = window.matchMedia( "(max-width: 992px)" );
+const md = window.matchMedia( "(min-width: 1000px)" );
+
 // Menu Slide
 
 	var menuHideShow = $('.nav-menu-res').children(".menu-hide").hasClass('show');
@@ -27,11 +30,44 @@
 		console.log(e.target)
 	})
 
-
-
-	// TEST
+	// Inmobiliaria items resize divs
 
 	$(".inmobiliaria__article").hover(function(){
-		$(this).children('.inmobiliaria__article_text').children('.inmobiliaria_article-hidden-text').toggleClass("inmobiliaria_article-hidden-text-show");
+		$(this).children('.inmobiliaria__article_text').children('.inmobiliaria__article-hidden-text').toggleClass("inmobiliaria_article-hidden-text-show");
 		$(this).toggleClass('inmobiliaria__article-resize');
 	});
+
+	// INMOBILIARIA PROYECTS RESIZE
+
+	if (sm.matches) {
+		$('.proyecto__item').click(function(e) {
+			var expanded = $(this);
+
+			$('.proyecto__item').not(this).toggleClass('proyecto__item-retract');
+			expanded.toggleClass('proyecto__item-expand');
+			expanded.children('.proyecto__item-content--mobile').toggleClass('proyecto__item-content--mobile-show');
+			expanded.children('.proyecto__item-title').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
+			e.stopPropagation();
+		})
+	}
+
+	if (md.matches) {
+		$('.proyecto__item').hover(function(e) {
+			var expanded = $(this);
+
+			$('.proyecto__item').not(this).toggleClass('proyecto__item-retract');
+			expanded.toggleClass('proyecto__item-expand');
+			expanded.children('.proyecto__item-title').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
+			e.stopPropagation();
+		})
+	}
+
+
+
+	// window.addEventListener('click', function(e) {
+	// 	if (expanded.hasClass('proyecto__item-expand') && e.target != expanded) {
+	// 		$('.proyecto__item').removeClass('proyecto__item-retract');
+	// 		$('.proyecto__item').removeClass('proyecto__item-expand');
+	// 		e.stopPropagation();
+	// 	}
+	// })

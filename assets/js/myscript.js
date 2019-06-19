06462f8128a28b1a2c487bfcf6338058d33df4a9
Matokosp <jmsalinas@uc.cl>
2019-06-19 00:29:00 -0400

    update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -46,7 +46,7 @@ const md = window.matchMedia( "(min-width: 1000px)" );
 			$('.proyecto__item').not(this).toggleClass('proyecto__item-retract');
 			expanded.toggleClass('proyecto__item-expand');
 			expanded.children('.proyecto__item-content--mobile').toggleClass('proyecto__item-content--mobile-show');
-			expanded.children('.proyecto__item-title').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
+			expanded.children('.proyecto__item-title--mobile').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
 			e.stopPropagation();
 		})
 	}
@@ -57,17 +57,35 @@ const md = window.matchMedia( "(min-width: 1000px)" );
 
 			$('.proyecto__item').not(this).toggleClass('proyecto__item-retract');
 			expanded.toggleClass('proyecto__item-expand');
-			expanded.children('.proyecto__item-title').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
+			expanded.children('.proyecto__item-title--mobile').children('.proyecto__item-title-h3').toggleClass('proyecto__item-title-h3-hide');
 			e.stopPropagation();
 		})
 	}
 
+	// MODAL BOX
 
+	// Get the modal
+	var modal = document.getElementById("myModal");
 
-	// window.addEventListener('click', function(e) {
-	// 	if (expanded.hasClass('proyecto__item-expand') && e.target != expanded) {
-	// 		$('.proyecto__item').removeClass('proyecto__item-retract');
-	// 		$('.proyecto__item').removeClass('proyecto__item-expand');
-	// 		e.stopPropagation();
-	// 	}
-	// })
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}

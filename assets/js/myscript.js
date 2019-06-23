d9b0372fee9de06eaab1e8922579635704004293
Matokosp <jmsalinas@uc.cl>
2019-06-23 02:23:46 -0400

    proyects content flexslider and modal box fix

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -119,43 +119,40 @@ const md = window.matchMedia( "(min-width: 1000px)" );
 
 	// MODAL BOX
 
-	// Get the modal
-	var modal = document.getElementById("myModal");
+	var btn = document.getElementsByClassName("myBtn");
 
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
+		for (var i = 0; i < btn.length; i++) {
+		  var thisBtn = btn[i];
+		  thisBtn.addEventListener("click", function(){
+		    var modal = document.getElementById(this.dataset.modal);
+		    modal.classList.add("modal-show");
+		}, false);
+	}
 
 	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementsByClassName("close");
 
-	// When the user clicks the button, open the modal
-	btn.onclick = function() {
-	  modal.style.display = "block";
-	}
+	// // When the user clicks on <span> (x), close the modal
+	$(span).on('click', function() {
+		$('.modal').removeClass('modal-show');
+	})
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
-	}
-
-	var initFlexSliders = function() {
-		$('.flexslider').flexslider({
-				animation: "slide",
-				slideshow: false,
-				controlNav: false,
-				directionNav: true,
-				start: function(slider){
-						$('.flexslider').resize();
-				}
-		});
-	};
-
-	//call this function to show the slider
-	initFlexSliders();
+	// // Get the modal
+	// var modal = document.getElementById("myModalOne");
+	// // Get the button that opens the modal
+	// var btn = document.getElementById("myBtn");
+	// // Get the <span> element that closes the modal
+	// var span = document.getElementsByClassName("close")[0];
+	// // When the user clicks the button, open the modal
+	// btn.onclick = function() {
+	//   modal.classList.add("modal-show");
+	// }
+	// // When the user clicks on <span> (x), close the modal
+	// span.onclick = function() {
+	//   modal.classList.remove("modal-show");	}
+	// // When the user clicks anywhere outside of the modal, close it
+	// window.onclick = function(event) {
+	//   if (event.target == modal) {
+	//     modal.style.display = "none";
+	//   }
+	// }
